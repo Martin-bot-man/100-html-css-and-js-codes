29db0c7bcae20e049bbf1a0fcdcc939f7d2ddb6c
Martin Odongo <martinwere14@gmail.com>
2024-10-14 14:58:03 +0300

    made changes on the event listeners

--- to do list/index.js
+++ to do list/index.js
@@ -1,13 +1,28 @@
-const itemsArray = localStorage.getItem('items')? JSON.parse(localStorage.getItem("items")):
-[]
-document.querySelector("#enter").addEventListener("click",()=>{
-    const item = document.querySelector("#item");
-    createItem(item)
+const itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+document.querySelector("#enter").addEventListener("click", () => {
+  const item = document.querySelector("#item")
+  createItem(item)
 })
+
+document.querySelector("#item").addEventListener("keypress", (e) => {
+  if(e.key === "Enter"){
+    const item = document.querySelector("#item")
+    createItem(item)
+  }
+})
+
+function displayDate(){
+  let date = new Date()
+  date = date.toString().split(" ")
+  date = date[1] + " " + date[2] + " " + date[3] 
+  document.querySelector("#date").innerHTML = date 
+}
+
 function displayItems(){
-    let items = ""
-    for(let i= 0;i< itemsArray.length;i++){
-    items+= `<div class="item">
+  let items = ""
+  for(let i = 0; i < itemsArray.length; i++){
+    items += `<div class="item">
                 <div class="input-controller">
                   <textarea disabled>${itemsArray[i]}</textarea>
                   <div class="edit-controller">
@@ -20,26 +35,74 @@ function displayItems(){
                   <button class="cancelBtn">Cancel</button>
                 </div>
               </div>`
-    }
-    document.querySelector(".to-do-list").innerHtml=items
+  }
+  document.querySelector(".to-do-list").innerHTML = items
+  activateDeleteListeners()
+  activateEditListeners()
+  activateSaveListeners()
+  activateCancelListeners()
 }
 
+function activateDeleteListeners(){
+  let deleteBtn = document.querySelectorAll(".deleteBtn")
+  deleteBtn.forEach((dB, i) => {
+    dB.addEventListener("click", () => { deleteItem(i) })
+  })
+}
+
+function activateEditListeners(){
+  const editBtn = document.querySelectorAll(".editBtn")
+  const updateController = document.querySelectorAll(".update-controller")
+  const inputs = document.querySelectorAll(".input-controller textarea")
+  editBtn.forEach((eB, i) => {
+    eB.addEventListener("click", () => { 
+      updateController[i].style.display = "block"
+      inputs[i].disabled = false })
+  })
+}
+
+function activateSaveListeners(){
+  const saveBtn = document.querySelectorAll(".saveBtn")
+  const inputs = document.querySelectorAll(".input-controller textarea")
+  saveBtn.forEach((sB, i) => {
+    sB.addEventListener("click", () => {
+      updateItem(inputs[i].value, i)
+    })
+  })
+}
+
+function activateCancelListeners(){
+  const cancelBtn = document.querySelectorAll(".cancelBtn")
+  const updateController = document.querySelectorAll(".update-controller")
+  const inputs = document.querySelectorAll(".input-controller textarea")
+  cancelBtn.forEach((cB, i) => {
+    cB.addEventListener("onclick", () => {
+      updateController[i].style.display = "none"
+      inputs[i].disabled = true
+      inputs[i].style.border = "none"
+    })
+  })
+}
 
 function createItem(item){
-    itemsArray.push(item.value);
-    localStorage.setItem("items",JSON.stringify(itemsArray))
-    location.reload()
+  itemsArray.push(item.value)
+  localStorage.setItem('items', JSON.stringify(itemsArray))
+  location.reload()
 }
 
-console.log(itemsArray)
-function displayDate(){
-    let date = new Date();
-    console.log(date);
-    date = date.toString().split(" ");
-    console.log(date);date
-    document.querySelector('#date').innerHTML= date[1]+' '+date[2]+' '+date[3];
+function deleteItem(i){
+  itemsArray.splice(i,1)
+  localStorage.setItem('items', JSON.stringify(itemsArray))
+  location.reload()
 }
-window.onload = function(){
-    displayDate();
-    displayItems()
+
+function updateItem(text, i){
+  itemsArray[i] = text
+  localStorage.setItem('items', JSON.stringify(itemsArray))
+  location.reload()
 }
+
+window.onload = function() {
+  displayDate()
+  displayItems()
+};
