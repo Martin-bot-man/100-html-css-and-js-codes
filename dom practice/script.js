6739353bc08a79c7abf13a13ac518fe649ba7216
Martin Odongo <martinwere14@gmail.com>
2024-10-16 17:51:53 +0300

    utilized the appendChild property

--- dom practice/script.js
+++ dom practice/script.js
@@ -1,11 +1,13 @@
 const addbtn = document.getElementById("add-button")
 addbtn.addEventListener('click' ,function addItem(){
-inputvalue= document.getElementById('input-text')
-if(!input.value==''){
+inputvalue = document.getElementById('input-text').value;
+if(inputvalue!==''){
 const list = document.createElement("li")
-const ul = document.getElementById("ul")
+const ul = document.getElementById("list-container")
 ul.appendChild(list)
 list.textContent = inputValue;
 
+//clear the input field
+document.getElementById('input-text').value='';
 }
 })
