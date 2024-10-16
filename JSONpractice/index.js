30f70ec3dcd5e6b20efa6ac8ede0f713ad2d9938
Martin Odongo <martinwere14@gmail.com>
2024-10-16 19:31:03 +0300

    learnt about session storage

--- JSONpractice/index.js
+++ JSONpractice/index.js
@@ -1,7 +1,15 @@
-//serializing javascript object to Json string
-const user = {name:'John Doe',age:30}
-const jsonString = JSON.stringify(user);
-console.log(jsonString);
-//parsing JSON string to javaSript object
-const jsonObject = JSON.parse(jsonString);
-console.log(jsonObject);
+// //serializing javascript object to Json string
+// const user = {name:'John Doe',age:30}
+// const jsonString = JSON.stringify(user);
+// console.log(jsonString);
+// //parsing JSON string to javaSript object
+// const jsonObject = JSON.parse(jsonString);
+// console.log(jsonObject);
+
+//storing data
+localStorage.setItem('username','Amanuel');
+//Retrieving data
+const username= localStorage.getItem('username');
+console.log(username);
+//Removing data
+localStorage.removeItem('username');
