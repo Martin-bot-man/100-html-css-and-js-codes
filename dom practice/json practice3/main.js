614556b679bf78039880712655d733b5799d8338
Martin Odongo <martinwere14@gmail.com>
2024-10-17 16:41:18 +0300

    practiced using fetch url

--- dom practice/json practice3/main.js
+++
@@ -1,9 +0,0 @@
-async function population(){
-    const requestURL = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
-    const request = new Request(requestURL);
-    const response = await fetch(request);
-    const superHeros = await response.json();
-
-    populateHeader(superHeros);
-    populateHeroes(superHeros);
-};
