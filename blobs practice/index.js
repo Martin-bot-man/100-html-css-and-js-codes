bae6cbe726c96bd62a5c8ca2665f54cec5d5a1a7
Martin Odongo <martinwere14@gmail.com>
2024-10-22 17:47:33 +0300

    added a href and a.download

--- blobs practice/index.js
+++ blobs practice/index.js
@@ -1,7 +1,8 @@
-const selector = document.querySelector("a");
-const data = "I love my mother";
-
-const blob = new Blob([data],{type:"text/plain"});
+const a = document.querySelector("a");
+const data = "I love both my mother and my father so much"
+ 
+const blob = new Blob([data],{type:"text/plain"}); 
 const url = URL.createObjectURL(blob);
 console.log(url);
-
+a.href = url;
+a.download = "blob-to-download.txt";
